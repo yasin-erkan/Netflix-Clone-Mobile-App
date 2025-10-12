4d66bea0d15d395de9ebc151ef67ae4f0cf923e8
Yasin Erkan <yasinerkan@gmail.com>
2025-10-13 00:00:57 +0200

    chore: Update dependencies and add project configuration files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,12 @@
 module.exports = {
   arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+  endOfLine: 'lf',
 };
